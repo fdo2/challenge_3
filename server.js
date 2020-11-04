63121193f2c35179febc097354c31d91b272027e
fdo2 <fdo2@live.com>
2020-11-04 14:35:41 +0200

    set & run the server

--- server.js
+++ server.js
@@ -1,2 +1,15 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+const port = process.env.PORT || 1990;
 
+app.listen(port, () => {
+  console.log(`Server is listening to http://localhost:${port}`)
+})
 
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.use(express.static(path.join(__dirname, 'client')));
